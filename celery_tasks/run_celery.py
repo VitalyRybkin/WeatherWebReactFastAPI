1517269143cf81e5898db194bf4669bf072ef32c
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-05-12 16:51:47 +0300

    feat: docker for prometheus and grafana

--- celery_tasks/run_celery.py
+++ celery_tasks/run_celery.py
@@ -2,10 +2,10 @@ from celery import Celery
 
 celery_app = Celery(
     "run_tasks",
-    broker="redis://redis:6379",
     # broker="redis://localhost:6379",
-    # backend="redis://redis:6379",
-    backend="redis://localhost:6379",
+    broker="redis://redis:6379",
+    backend="redis://redis:6379",
+    # backend="redis://localhost:6379",
     include=["celery_tasks.tasks"],
     ignore_result=False,
 )
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from prometheus_client import make_asgi_app, Counter
 
 from api_v1.views import location_router
 from models import AbstractBaseModel
-from users.user_router import user_router
 from users.settings_router import settings_router
+from users.user_router import user_router
 from utils.db_engine import db_engine
 
 
@@ -21,6 +22,17 @@ app = FastAPI(lifespan=lifespan, root_path="/app")
 app.include_router(user_router, tags=["users"])
 app.include_router(settings_router, tags=["settings"])
 app.include_router(location_router, tags=["locations"])
+
+index_counter = Counter("index_counter", "Description of counter")
+
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
+
+
+@app.get("/")
+def index():
+    index_counter.inc()
+    return "Wellcome to the weather forecast world!"
 
 
 if __name__ == "__main__":
