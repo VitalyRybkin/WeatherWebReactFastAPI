bfe9ff2d0ce00c86aaf49f3c225df54d34d781b8
vitaliyrybkin <vitaliy.rybkin@yandex.ru>
2025-06-09 17:22:38 +0300

    fix: app response class

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 from prometheus_client import make_asgi_app
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api_v1.views import location_router
-from app.models.base import AbstractBaseModel
 from app.users.settings_router import settings_router
 from app.users.user_router import user_router
 from app.utils.db_engine import db_engine
@@ -30,7 +30,7 @@ async def lifespan(app: FastAPI):  # pylint: disable=W0613, W0621
     db_engine.dispose()
 
 
-app = FastAPI(lifespan=lifespan, root_path="/app")
+app = FastAPI(lifespan=lifespan, root_path="/app", response_class=ORJSONResponse)
 app.include_router(user_router, tags=["users"])
 app.include_router(settings_router, tags=["settings"])
 app.include_router(location_router, tags=["locations"])
